6e9e7d79ea36d368a23452e7ec07b79ede09ea24
twostars <i.am.twostars@gmail.com>
2025-06-30 04:15:10 +1000

    CUILevelGuide should not need to explicitly hide/close
    Its style -- UISTYLE_POS_RIGHT -- takes care of this.

--- Client/WarFare/UIStateBar.cpp
+++ Client/WarFare/UIStateBar.cpp
@@ -15,6 +15,7 @@
 #include "UILevelGuide.h"
 #include "N3UIDBCLButton.h"
 
+#include <N3Base/N3UIButton.h>
 #include <N3Base/N3UIProgress.h>
 #include <N3Base/N3UIString.h>
 #include <N3Base/N3UIImage.h>
@@ -575,26 +576,24 @@ bool CUIStateBar::ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg)
 {
 	if (dwMsg == UIMSG_BUTTON_CLICK)
 	{
-		if (pSender == (CN3UIBase*)m_pBtn_ZoomIn)
+		if (pSender == m_pBtn_ZoomIn)
 		{
 			ZoomSet(m_fZoom*1.1f);
 			return true;
 		}
-		else if (pSender == (CN3UIBase*)m_pBtn_ZoomOut)
+		else if (pSender == m_pBtn_ZoomOut)
 		{
 			ZoomSet(m_fZoom*0.9f);
 			return true;
 		}
-		else if (pSender == (CN3UIBase*)m_pBtn_Quest)
+		else if (pSender == m_pBtn_Quest)
 		{
 			if (CGameProcedure::s_pProcMain->m_pUILevelGuide != nullptr)
-			{
 				CGameProcedure::s_pProcMain->CommandToggleLevelGuide();
-			}
 
 			return true;
 		}
-		else if (pSender == (CN3UIBase*)m_pBtn_Power)
+		else if (pSender == m_pBtn_Power)
 		{
 			//TODO: Launch msgbox with warning message, on confirm -> open store (Web_Browser UI or maybe external?)
 			return false;
