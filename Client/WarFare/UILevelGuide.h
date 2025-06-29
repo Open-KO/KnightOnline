f974e4cf36013d6cefed1ebadb2e9ef2c7272923
twostars <i.am.twostars@gmail.com>
2025-06-30 04:07:40 +1000

    Reformat and very minor refactoring
    Modernise basic constructs with std::clamp(), etc.

--- Client/WarFare/UILevelGuide.h
+++ Client/WarFare/UILevelGuide.h
@@ -14,41 +14,40 @@
 class CUILevelGuide : public CN3UIBase
 {
 protected:
-	CN3UIEdit* m_pEditLevel;
-	CN3UIScrollBar* m_pScrollGuide2;
-	CN3UIScrollBar* m_pScrollGuide1;
-	CN3UIScrollBar* m_pScrollGuide0;
-	CN3UIString* m_pTextPage;
-	//CN3UIString* m_pTextInfo; //static, no id
-	CN3UIButton* m_pButtonCheck;
-	CN3UIString* m_pTextGuide2;
-	CN3UIString* m_pTextGuide1;
-	CN3UIString* m_pTextGuide0;
-	CN3UIString* m_pTextTitle2;
-	CN3UIString* m_pTextTitle1;
-	CN3UIString* m_pTextTitle0;
-	CN3UIString* m_pTextLevel;
-	CN3UIButton* m_pButtonUp;
-	CN3UIButton* m_pButtonDown;
-	CN3UIButton* m_pButtonCancel;
+	static constexpr int SEARCH_RANGE			= 5;		// ex: searches 5 levels up and down
+	static constexpr int MAX_LIST_SIZE			= 20;		// max list size
+	static constexpr int VISIBLE_ENTRY_COUNT	= 3;
+	static constexpr int MAX_PAGE_COUNT
+		= (MAX_LIST_SIZE / VISIBLE_ENTRY_COUNT) + ((MAX_LIST_SIZE % VISIBLE_ENTRY_COUNT) != 0 ? 1 : 0);
 
-	static constexpr int SEARCH_RANGE = 5; //ex: searches 5 levels up and down
-	static constexpr int CONTENT_COUNT = 20; //number of contents, equal to m_saQuestTitle,m_saQuestText size
-	int m_iSearchLevel;
-	int m_iCurrentPage;
-	std::string m_saQuestTitle[CONTENT_COUNT];
-	std::string m_saQuestText[CONTENT_COUNT];
-	void SetTopLine(CN3UIScrollBar* pScroll, CN3UIString* pTextGuide);
+	CN3UIEdit*		m_pEdit_Level;
+	CN3UIScrollBar*	m_pScroll_Guide[VISIBLE_ENTRY_COUNT];
+	CN3UIString*	m_pText_Guide[VISIBLE_ENTRY_COUNT];
+	CN3UIString*	m_pText_Title[VISIBLE_ENTRY_COUNT];
+	CN3UIString*	m_pText_Page;
+	CN3UIButton*	m_pBtn_Check;
+	CN3UIString*	m_pText_Level;
+	CN3UIButton*	m_pBtn_Up;
+	CN3UIButton*	m_pBtn_Down;
+	CN3UIButton*	m_pBtn_Cancel;
+
+	int				m_iSearchLevel;
+	int				m_iCurrentPage;
+	std::string		m_saQuestTitle[MAX_LIST_SIZE];
+	std::string		m_saQuestText[MAX_LIST_SIZE];
+
 public:
-	bool Load(HANDLE hFile) override;
-	void Release() override;
 	CUILevelGuide();
 	~CUILevelGuide() override;
-	void SetVisible(bool bVisible);
-	bool ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg);
-	bool OnKeyPress(int iKey);
+	bool Load(HANDLE hFile) override;
+	void Release() override;
+	void SetVisible(bool bVisible) override;
+	bool ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg) override;
+	bool OnKeyPress(int iKey) override;
 	void LoadContent();
 
+protected:
+	void SetTopLine(CN3UIScrollBar* pScroll, CN3UIString* pTextGuide);
 };
 
 #endif // !defined(AFX_UIStateBar_H__C1BBB503_F9E5_43BB_93CB_C542AC016F85__INCLUDED_)
