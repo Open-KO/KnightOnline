66b66438ab66e704536bc0489a7aa33fd0d7e0e6
koraykarakus <koraykarakus@yahoo.com>
2025-08-20 14:08:59 +0300

    negative check for gold, negative check for user gold, return type to bool from BOOL

--- Server/Ebenezer/User.cpp
+++ Server/Ebenezer/User.cpp
@@ -11638,14 +11638,20 @@ void CUser::GoldGain(int gold)
 	Send(send_buff, send_index);
 }
 
-BOOL CUser::GoldLose(int gold)
+bool CUser::GoldLose(int gold)
 {
 	int send_index = 0;
 	char send_buff[256] = {};
 
+	if (m_pUserData->m_iGold < 0)
+		m_pUserData->m_iGold = 0;
+
+	if (gold < 0)
+		gold = 0;
+
 	// Insufficient gold!
 	if (m_pUserData->m_iGold < gold)
-		return FALSE;
+		return false;
 
 	m_pUserData->m_iGold -= gold;	// Subtract gold.
 
@@ -11655,7 +11661,7 @@ BOOL CUser::GoldLose(int gold)
 	SetDWORD(send_buff, m_pUserData->m_iGold, send_index);
 	Send(send_buff, send_index);
 
-	return TRUE;
+	return true;
 }
 
 BOOL CUser::CheckSkillPoint(BYTE skillnum, BYTE min, BYTE max)
