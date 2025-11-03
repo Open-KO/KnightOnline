debcb56b424cfed8a080de22911eeebf5b4857d6
twostars <i.am.twostars@gmail.com>
2025-11-03 17:25:04 +1100

    Strip unnecessary includes

--- Server/AIServer/pch.h
+++ Server/AIServer/pch.h
@@ -2,34 +2,12 @@
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
-
-// #if !defined(AFX_STDAFX_H__7F6B8F8D_6B2B_47EE_A744_F49F1D4A2556__INCLUDED_)
-// #define AFX_STDAFX_H__7F6B8F8D_6B2B_47EE_A744_F49F1D4A2556__INCLUDED_
-//
-// #if _MSC_VER > 1000
 #pragma once
-// #endif // _MSC_VER > 1000
-
-// #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <asio.hpp>
 
-// #include <afxwin.h>         // MFC core and standard components
-// #include <afxext.h>         // MFC extensions
-// #include <afxdisp.h>        // MFC Automation classes
-// #include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
-// #ifndef _AFX_NO_AFXCMN_SUPPORT
-// #include <afxcmn.h>			// MFC support for Windows Common Controls
-// #endif // _AFX_NO_AFXCMN_SUPPORT
-//
-// #include <afxtempl.h>
-// #include <afxdb.h>
-#include <sql.h>
-#pragma comment(lib, "odbc32.lib")
-#include <atlexcept.h>
-
-#include "packet.h"			// packet 정의
-#include "global.h"			// 전역 funtion
-#include "Define.h"			// define
+#include "Packet.h"
+#include "global.h"
+#include "Define.h"
 
 #include <shared-server/server_config.h>
