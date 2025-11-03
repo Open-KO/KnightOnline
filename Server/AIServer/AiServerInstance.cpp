669050a45cad95ad1a1b6c64a63133e33ebaaf22
twostars <i.am.twostars@gmail.com>
2025-11-04 09:12:00 +1100

    AiServerInstance::thread_loop: Wait on _cv instead

--- Server/AIServer/AiServerInstance.cpp
+++ Server/AIServer/AiServerInstance.cpp
@@ -353,9 +353,8 @@ void AiServerInstance::thread_loop()
 		
 	while (_canTick)
 	{
-		// check for threads that may need to be restarted
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::unique_lock<std::mutex> lock(_mutex);
+		_cv.wait(lock);
 	}
 }
 
