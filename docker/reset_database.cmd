75724d92bdf56529bc5fe0449e54a4ae962f3484
kenner2 <13474249+kenner2@users.noreply.github.com>
2025-12-05 12:06:34 -0500

    * Update defaults to use KN_online rather than KN_Online * Update Linux scripts to force their working directories to their script folder.  Windows is still TODO * twostars doesn't like my jokes * Cleanup in docker/README.MD

--- docker/reset_database.cmd
+++ docker/reset_database.cmd
@@ -3,7 +3,7 @@ cd ..
 
 :: Performs a clean import of the OpenKO database.  This will 
 :: remove any existing OpenKO database instance matching the 
-:: DB_NAME from the .env file (default: KN_Online), then 
+:: DB_NAME from the .env file (default: KN_online), then 
 :: create a replacement database using the latest data 
 :: from the OpenKO-db project.
 docker exec knightonline-kodb-util-1 /usr/local/bin/cleanImport.sh
