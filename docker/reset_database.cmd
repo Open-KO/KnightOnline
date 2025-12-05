81df0b9e06387338a1c46097a5317fb9688c8e81
kenner <13474249+kenner2@users.noreply.github.com>
2025-12-05 12:30:24 -0500

    * Update Windows helper scripts to find their directory on the file system and use it as a working directory. * Update the number of retries on the healthcheck for sqlserver service to allow for more time on slower machines

--- docker/reset_database.cmd
+++ docker/reset_database.cmd
@@ -1,5 +1,13 @@
 :: Windows script
+:: Find script location on FS and use that as the working directory
+:: %~dp0: drive letter + back‑slash + directory
+set "SCRIPT_DIR=%~dp0"
+pushd "%SCRIPT_DIR%" || (
+    echo Failed to change to directory: "%SCRIPT_DIR%"
+    exit /b 1
+)
 cd ..
+echo Working directory set to: %CD%
 
 :: Performs a clean import of the OpenKO database.  This will 
 :: remove any existing OpenKO database instance matching the 
--- docker/stop_containers.cmd
+++ docker/stop_containers.cmd
@@ -1,5 +1,13 @@
 :: Windows script
+:: Find script location on FS and use that as the working directory
+:: %~dp0: drive letter + back‑slash + directory
+set "SCRIPT_DIR=%~dp0"
+pushd "%SCRIPT_DIR%" || (
+    echo Failed to change to directory: "%SCRIPT_DIR%"
+    exit /b 1
+)
 cd ..
+echo Working directory set to: %CD%
 
 :: Stop the containers without removing any images or volumes
 :: resume with resume_containers.sh
