98fa3adba486b27d4cdb7346440fc703d4016a72
twostars <i.am.twostars@gmail.com>
2025-12-31 02:15:42 +1100

    Strip DX9 SDK implementation so we can simplify include paths
    With the property sheet no longer needed, we don't necessarily require the VS generator anymore,
    though this isn't strictly tested/supported.

--- cmake/dx9sdk/dx9sdk-config.cmake
+++ cmake/dx9sdk/dx9sdk-config.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 fetchcontent_declare(
   dx9sdk
   GIT_REPOSITORY        "https://github.com/Open-KO/microsoft-directx-sdk.git"
-  GIT_TAG               "dx9-june-2010"
+  GIT_TAG               "v9.0-OpenKO"
   GIT_PROGRESS          ON
   GIT_SHALLOW           ON
   SOURCE_DIR            "${FETCHCONTENT_BASE_DIR}/dx9sdk"
@@ -53,6 +53,11 @@ target_compile_definitions(dx9sdk INTERFACE
   "DIRECTINPUT_VERSION=0x0800"
 )
 
+# Expose include path
+target_include_directories(dx9sdk INTERFACE
+  "${DX9_INCLUDE_DIR}"
+)
+
 # Link in the libs that we use
 target_link_libraries(dx9sdk INTERFACE
   "${DX9_LIBRARY_DIR}/d3d9.lib"
@@ -63,35 +68,3 @@ target_link_libraries(dx9sdk INTERFACE
   "${DX9_LIBRARY_DIR}/dxguid.lib"
 )
 
-# We need a very explicit include order for DX9, because of its awkward header conflicts.
-#
-# To ensure this, we must append it to IncludePath (not AdditionalIncludeDirectories).
-# This is technically exposed via VS_GLOBAL_IncludePath, but it's inserted before the
-# VS defaults are actually added, which would prepend rather than append if VS didn't
-# outright not bother to set it up because it has a value already.
-#
-# Instead, we can use a property sheet. This replaces the VS user default prop sheet
-# (which we must reimport ourselves), but is added late enough that we can append to
-# the existing IncludePath setting and ensure our path is consistent.
-#
-# The only problem with this approach is that we cannot propagate this to consumers.
-# So we add require_directx9() to force consumers to give themselves the property sheet
-# manually.
-set(DIRECTX9_PROPS_TEMPLATE "${OPENKO_MODULE_PATH}/dx9sdk/directx9.props.template")
-set(DIRECTX9_PROPS_DIR "${CMAKE_CURRENT_BINARY_DIR}/props")
-set(DIRECTX9_PROPS_PATH "${DIRECTX9_PROPS_DIR}/directx9.props" CACHE STRING "DirectX 9 property sheet path for includes (internal)" FORCE)
-
-# Make sure the target directory exists
-file(MAKE_DIRECTORY "${DIRECTX9_PROPS_DIR}")
-
-configure_file("${DIRECTX9_PROPS_TEMPLATE}" "${DIRECTX9_PROPS_PATH}" @ONLY)
-
-function(require_directx9 TARGET)
-  if(NOT TARGET ${TARGET})
-    message(FATAL_ERROR "Target ${TARGET} does not exist!")
-  endif()
-
-  if(MSVC)
-    set_target_properties(${TARGET} PROPERTIES VS_USER_PROPS "${DIRECTX9_PROPS_PATH}")
-  endif()
-endfunction()
