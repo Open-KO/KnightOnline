1b2dadd5faf28bf287db21f9c7058f4e2ba9d013
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2026-01-06 11:52:38 +0300

    Update N3TexViewerDoc.cpp

--- src/Tools/N3TexViewer/N3TexViewerDoc.cpp
+++ src/Tools/N3TexViewer/N3TexViewerDoc.cpp
@@ -1,4 +1,4 @@
-﻿// N3TexViewerDoc.cpp : implementation of the CN3TexViewerDoc class
+// N3TexViewerDoc.cpp : implementation of the CN3TexViewerDoc class
 //
 
 #include "stdafx.h"
@@ -19,9 +19,9 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNCREATE(CN3TexViewerDoc, CDocument)
 
 BEGIN_MESSAGE_MAP(CN3TexViewerDoc, CDocument)
-//{{AFX_MSG_MAP(CN3TexViewerDoc)
-ON_COMMAND(ID_FILE_SAVE_AS_BITMAP, OnFileSaveAsBitmap)
-//}}AFX_MSG_MAP
+	//{{AFX_MSG_MAP(CN3TexViewerDoc)
+	ON_COMMAND(ID_FILE_SAVE_AS_BITMAP, OnFileSaveAsBitmap)
+	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -30,10 +30,10 @@ END_MESSAGE_MAP()
 CN3TexViewerDoc::CN3TexViewerDoc()
 {
 	// TODO: add one-time construction code here
-	m_pTex      = new CN3Texture();
+	m_pTex = new CN3Texture();
 	m_pTexAlpha = new CN3Texture();
 
-	m_nCurFile  = 0;
+	m_nCurFile = 0;
 }
 
 CN3TexViewerDoc::~CN3TexViewerDoc()
@@ -71,9 +71,6 @@ void CN3TexViewerDoc::Dump(CDumpContext& dc) const
 }
 #endif //_DEBUG
 
-/////////////////////////////////////////////////////////////////////////////
-// CN3TexViewerDoc commands
-
 BOOL CN3TexViewerDoc::OnNewDocument()
 {
 	if (!CDocument::OnNewDocument())
@@ -94,215 +91,197 @@ BOOL CN3TexViewerDoc::OnOpenDocument(LPCTSTR lpszPathName)
 	if (!CDocument::OnOpenDocument(lpszPathName))
 		return FALSE;
 
-	this->FindFiles(); // 파일을 찾고..
-
-	// TODO: Add your specialized creation code here
-	m_pTexAlpha->Release();
 	if (!m_pTex->LoadFromFile(lpszPathName))
 		return FALSE;
 
-	////////////////////////////////////////////////////////////////////////////////
-	// Alpha Texture 생성...
-	m_pTexAlpha->Create(m_pTex->Width(), m_pTex->Height(), D3DFMT_A8R8G8B8, FALSE);
-	LPDIRECT3DSURFACE9 lpSurf, lpSurf2;
-	if (m_pTexAlpha->Get())
-	{
-		m_pTexAlpha->Get()->GetSurfaceLevel(0, &lpSurf);
-		m_pTex->Get()->GetSurfaceLevel(0, &lpSurf2);
-		::D3DXLoadSurfaceFromSurface(
-			lpSurf, nullptr, nullptr, lpSurf2, nullptr, nullptr, D3DX_FILTER_TRIANGLE, 0);
-		lpSurf2->Release();
-		lpSurf2 = nullptr;
+	FindFiles(lpszPathName);
 
-		D3DLOCKED_RECT LR;
-		lpSurf->LockRect(&LR, nullptr, 0);
-		int width = m_pTexAlpha->Width(), height = m_pTexAlpha->Height();
-		DWORD dwAlpha = 0;
-		DWORD* pPixel = nullptr;
-		for (int y = 0; y < height; y++)
+	if (m_pTexAlpha) m_pTexAlpha->Release();
+
+	D3DFORMAT fmt = m_pTex->PixelFormat();
+	if (fmt == D3DFMT_DXT3 || fmt == D3DFMT_DXT5 || fmt == D3DFMT_A8R8G8B8)
+	{
+		m_pTexAlpha->Create(m_pTex->Width(), m_pTex->Height(), D3DFMT_A8R8G8B8, FALSE);
+
+		LPDIRECT3DSURFACE9 lpSurf = nullptr, lpSurf2 = nullptr;
+		if (m_pTexAlpha && m_pTexAlpha->Get())
 		{
-			for (int x = 0; x < width; x++)
+			if (SUCCEEDED(m_pTexAlpha->Get()->GetSurfaceLevel(0, &lpSurf)) &&
+				SUCCEEDED(m_pTex->Get()->GetSurfaceLevel(0, &lpSurf2)))
 			{
-				pPixel  = (DWORD*) LR.pBits + y * (LR.Pitch / 4) + x;
-				dwAlpha = (*pPixel) >> 24;
-				*pPixel = dwAlpha | (dwAlpha << 8) | (dwAlpha << 16) | 0xff000000;
+				if (SUCCEEDED(::D3DXLoadSurfaceFromSurface(lpSurf, nullptr, nullptr,
+					lpSurf2, nullptr, nullptr,
+					D3DX_FILTER_TRIANGLE, 0)))
+				{
+					D3DLOCKED_RECT LR;
+					if (SUCCEEDED(lpSurf->LockRect(&LR, nullptr, 0)))
+					{
+						const int width = m_pTexAlpha->Width();
+						const int height = m_pTexAlpha->Height();
+
+						for (int y = 0; y < height; ++y)
+						{
+							DWORD* pRow = reinterpret_cast<DWORD*>((BYTE*)LR.pBits + y * LR.Pitch);
+							for (int x = 0; x < width; ++x)
+							{
+								DWORD dwAlpha = (pRow[x] >> 24) & 0xFF;
+								pRow[x] = (dwAlpha << 24) | (dwAlpha << 16) | (dwAlpha << 8) | dwAlpha;
+							}
+						}
+						lpSurf->UnlockRect();
+					}
+				}
+				if (lpSurf2) { lpSurf2->Release(); lpSurf2 = nullptr; }
+				if (lpSurf) { lpSurf->Release();  lpSurf = nullptr; }
 			}
 		}
-		lpSurf->UnlockRect();
-		lpSurf->Release();
-		lpSurf = nullptr;
 	}
-	// Alpha Texture 생성...
-	////////////////////////////////////////////////////////////////////////////////
 
 	char szDrv[_MAX_DRIVE], szDir[_MAX_DIR], szFN[_MAX_FNAME], szExt[_MAX_EXT];
 	::_splitpath(lpszPathName, szDrv, szDir, szFN, szExt);
-	CString szFileName  = szFN;
-	szFileName         += szExt;
+	CString szFileName = szFN; szFileName += szExt;
+	SetTitle(szFileName);
 
-	this->SetTitle(szFileName);
-	this->UpdateAllViews(nullptr);
-
+	UpdateAllViews(nullptr);
 	return TRUE;
 }
 
-BOOL CN3TexViewerDoc::OnSaveDocument(LPCTSTR lpszPathName)
+
+BOOL CN3TexViewerDoc::OnSaveDocument(LPCTSTR lpszPathName) 
 {
 	char szDrv[_MAX_DRIVE], szDir[_MAX_DIR], szFN[_MAX_FNAME], szExt[_MAX_EXT];
 	::_splitpath(lpszPathName, szDrv, szDir, szFN, szExt);
 
-	if (lstrcmpi(szExt, ".DXT") == 0) // 확장자가 DXT 면 그냥 저장..
+	if(lstrcmpi(szExt, ".DXT") == 0 || lstrcmpi(szExt, ".dxt") == 0 || lstrcmpi(szExt, ".gtt") == 0) // 확장자가 DXT 면 그냥 저장..
 	{
 		CDocument::OnSaveDocument(lpszPathName);
 
-		if (false == m_pTex->SaveToFile(lpszPathName))
-			return FALSE;
+		if(false == m_pTex->SaveToFile(lpszPathName)) return FALSE;
 
 		return TRUE;
 	}
-	else
+	else 
 	{
-		MessageBox(::GetActiveWindow(), "확장자를 DXT 로 바꾸어야 합니다. Save As 로 저장해주세요.",
-			"저장 실패", MB_OK);
+		MessageBox(::GetActiveWindow(), "확장자를 DXT 로 바꾸어야 합니다. Save As 로 저장해주세요.", "저장 실패", MB_OK);
 
 		return FALSE;
 	}
 }
 
-void CN3TexViewerDoc::SetTitle(LPCTSTR lpszTitle)
+void CN3TexViewerDoc::SetTitle(LPCTSTR lpszTitle) 
 {
-	// TODO: Add your specialized code here and/or call the base class
 	CString szFmt;
 	szFmt.Format("%s - %d, %d", lpszTitle, m_pTex->Width(), m_pTex->Height());
-	D3DFORMAT fmtTex = m_pTex->PixelFormat();
-	if (D3DFMT_DXT1 == fmtTex)
-		szFmt += " DXT1";
-	else if (D3DFMT_DXT2 == fmtTex)
-		szFmt += " DXT2";
-	else if (D3DFMT_DXT3 == fmtTex)
-		szFmt += " DXT3";
-	else if (D3DFMT_DXT4 == fmtTex)
-		szFmt += " DXT4";
-	else if (D3DFMT_DXT5 == fmtTex)
-		szFmt += " DXT5";
-	else if (D3DFMT_A1R5G5B5 == fmtTex)
-		szFmt += " A1R5G5B5";
-	else if (D3DFMT_A4R4G4B4 == fmtTex)
-		szFmt += " A4R4G4B4";
-	else if (D3DFMT_R8G8B8 == fmtTex)
-		szFmt += " R8G8B8";
-	else if (D3DFMT_A8R8G8B8 == fmtTex)
-		szFmt += " A8R8G8B8";
-	else if (D3DFMT_X8R8G8B8 == fmtTex)
-		szFmt += " X8R8G8B8";
-	else
-		szFmt += " Unknown Format";
 
-	if (m_pTex->MipMapCount() > 1)
-		szFmt += " - has MipMap";
-	else
-		szFmt += " - has no MipMap";
+	switch (m_pTex->PixelFormat()) { 
+		case D3DFMT_DXT1: szFmt += " DXT1"; break;
+		case D3DFMT_DXT2: szFmt += " DXT2"; break; 
+		case D3DFMT_DXT3: szFmt += " DXT3"; break; 
+		case D3DFMT_DXT4: szFmt += " DXT4"; break; 
+		case D3DFMT_DXT5: szFmt += " DXT5"; break; 
 
+		case D3DFMT_A1R5G5B5: szFmt += " A1R5G5B5"; break; 
+		case D3DFMT_A4R4G4B4: szFmt += " A4R4G4B4"; break; 
+		case D3DFMT_R8G8B8: szFmt += " R8G8B8"; break; 
+		case D3DFMT_A8R8G8B8: szFmt += " A8R8G8B8"; break; 
+		case D3DFMT_X8R8G8B8: szFmt += " X8R8G8B8"; break;
+		default: szFmt += " Unknown Format"; break;
+	}
+
+	szFmt += (m_pTex->MipMapCount() > 1) ? " - has MipMap" : " - has no MipMap";
 	CDocument::SetTitle(szFmt);
-	//	CDocument::SetTitle(lpszTitle);
 }
 
-void CN3TexViewerDoc::FindFiles()
+void CN3TexViewerDoc::FindFiles(LPCTSTR lpszPathName)
 {
-	char szPath[_MAX_PATH];
-	GetCurrentDirectory(_MAX_PATH, szPath);
+	if (!lpszPathName || _tcslen(lpszPathName) == 0) return;
 
-	CString szPath2 = szPath;
+	CString filePath = lpszPathName; 
+	int pos = filePath.ReverseFind('\\');
+	if (pos == -1) return;
+
+	CString dirPath = filePath.Left(pos); 
+	CString szPath2 = dirPath; 
 	szPath2.MakeLower();
 
-	if (m_szPath == szPath2)
-		return;
+	if (m_szPath == szPath2) return;
 
 	m_szPath = szPath2;
 	m_szFiles.RemoveAll();
-
-	CFileFind find;
-
-	int i      = 0;
 	m_nCurFile = 0;
-	if (FALSE == find.FindFile("*.DXT"))
-		return;
-
-	for (i = 0; find.FindNextFile(); i++)
+	 
+	const char* patterns[] = { "*.DXT", "*.GTT" };
+	for (auto& pat : patterns)
 	{
-		CString szPathTmp = find.GetFilePath();
-		m_szFiles.Add(szPathTmp);
+		CFileFind finder;
+		CString searchPattern = dirPath + "\\" + pat;
+		BOOL bFound = finder.FindFile(searchPattern);
+		while (bFound)
+		{
+			bFound = finder.FindNextFile();
 
-		if (szPathTmp == this->GetPathName())
-			m_nCurFile = i;
+			if (!finder.IsDots() && !finder.IsDirectory())
+			{
+				CString szPathTmp = finder.GetFilePath();
+				m_szFiles.Add(szPathTmp);
+
+				if (szPathTmp.CompareNoCase(filePath) == 0) 
+					m_nCurFile = static_cast<int>(m_szFiles.GetSize() - 1);
+			}
+		}
+		finder.Close();
 	}
 }
 
 void CN3TexViewerDoc::OpenNextFile()
 {
-	m_nCurFile++;
-
-	int nFC = static_cast<int>(m_szFiles.GetSize());
-	if (m_nCurFile < 0 || m_nCurFile >= nFC)
-	{
-		m_nCurFile = nFC - 1;
-		return;
-	}
-
+	if (m_szFiles.IsEmpty()) return; 
+	
+	if (m_nCurFile + 1 < m_szFiles.GetSize())
+		m_nCurFile++; 
+	
 	OnOpenDocument(m_szFiles[m_nCurFile]);
 }
 
 void CN3TexViewerDoc::OpenPrevFile()
 {
-	m_nCurFile--;
-
-	int nFC = static_cast<int>(m_szFiles.GetSize());
-	if (m_nCurFile < 0 || m_nCurFile >= nFC)
-	{
-		m_nCurFile = 0;
-		return;
-	}
-
+	if (m_szFiles.IsEmpty()) return; 
+	
+	if (m_nCurFile > 0) 
+		m_nCurFile--; 
+	
 	OnOpenDocument(m_szFiles[m_nCurFile]);
 }
 
 void CN3TexViewerDoc::OpenFirstFile()
 {
-	m_nCurFile = 0;
-
-	int nFC    = static_cast<int>(m_szFiles.GetSize());
-	if (m_nCurFile < 0 || m_nCurFile >= nFC)
-	{
-		m_nCurFile = 0;
+	if (m_szFiles.IsEmpty()) 
 		return;
-	}
+
+	m_nCurFile = 0;
 
 	OnOpenDocument(m_szFiles[m_nCurFile]);
 }
 
 void CN3TexViewerDoc::OpenLastFile()
 {
-	int nFC    = static_cast<int>(m_szFiles.GetSize());
-	m_nCurFile = nFC - 1;
-
-	if (m_nCurFile < 0 || m_nCurFile >= nFC)
-	{
-		m_nCurFile = 0;
+	if (m_szFiles.IsEmpty()) 
 		return;
-	}
+
+	m_nCurFile = static_cast<int>(m_szFiles.GetSize()) - 1;
 
 	OnOpenDocument(m_szFiles[m_nCurFile]);
 }
 
 void CN3TexViewerDoc::OnFileSaveAsBitmap()
 {
-	if (nullptr == m_pTex || nullptr == m_pTex->Get())
-		return;
+	if (nullptr == m_pTex || nullptr == m_pTex->Get()) return;
 
-	DWORD dwFlags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_LONGNAMES | OFN_PATHMUSTEXIST;
-	CFileDialog dlg(FALSE, "bmp", nullptr, dwFlags, "Bitmap file(*.bmp)|*.bmp||", nullptr);
-	if (dlg.DoModal() == IDCANCEL)
-		return;
+	CFileDialog dlg(FALSE, _T("bmp"), nullptr,
+		OFN_EXPLORER | OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY,
+		_T("Bitmap file (*.bmp)|*.bmp||"), nullptr);
+
+	if (dlg.DoModal() != IDOK) return;
 
 	CString szPath = dlg.GetPathName();
 	CT2A szPathA(szPath);
