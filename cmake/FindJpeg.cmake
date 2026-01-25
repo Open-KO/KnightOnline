00a96bc98c9330e6fb7231346ba1b8cf3d9fde33
twostars <i.am.twostars@gmail.com>
2026-01-26 07:52:14 +1100

    Replace libjpeg-turbo with IJG's libjpeg
    libjpeg-turbo is a bad fit as a CMake dependency.
    
    It requires the use of ExternalProject_Add() which creates complications,
    particularly when swapping the compiler (e.g. for mingw), not to mention
    as-is, it ends up having to fetch at build time rather than configure time
    like every other dependency.
    
    As we only use this for screenshots (KSCs) (i.e. its use is limited to the
    KscViewer and the very rare instance of pressing the in-game screenshot button),
    the performance benefit of libjpeg-turbo is fairly moot.
    
    For simplicity, we go back to the basic IJG implementation (with unofficially
    added CMake config).
    This allows us to use the dependency directly and consistently with the others,
    and there's no concern with it using a different compiler unintentionally.

---
+++ cmake/FindJpeg.cmake
@@ -0,0 +1,34 @@
+# Get Jpeg package
+#
+# Makes the libjpeg target available.
+
+fetchcontent_declare(
+  jpeg
+  GIT_REPOSITORY        "https://github.com/Open-KO/jpeg-cmake.git"
+  GIT_TAG               "v1.3.0a-OpenKO"
+  GIT_PROGRESS          ON
+  GIT_SHALLOW           ON
+  EXCLUDE_FROM_ALL
+)
+
+if(WIN32)
+  set(LIBJPEG_BUILD_SHARED_LIBS OFF CACHE BOOL "libjpeg: Build shared libraries")
+  set(LIBJPEG_BUILD_STATIC_LIBS ON CACHE BOOL "libjpeg: Build static libraries")
+else()
+  set(LIBJPEG_BUILD_SHARED_LIBS ON CACHE BOOL "libjpeg: Build shared libraries")
+  set(LIBJPEG_BUILD_STATIC_LIBS OFF CACHE BOOL "libjpeg: Build static libraries")
+endif()
+
+set(LIBJPEG_INSTALL OFF CACHE BOOL "libjpeg: Install targets and headers")
+
+fetchcontent_makeavailable(jpeg)
+
+# Setup a wrapper project to reference the preferred zlib target for this build
+add_library(libjpeg INTERFACE)
+if(WIN32)
+  target_link_libraries(libjpeg INTERFACE jpeg_static)
+else()
+  target_link_libraries(libjpeg INTERFACE jpeg)
+endif()
+
+set(jpeg_FOUND TRUE)
